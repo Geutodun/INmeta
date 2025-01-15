43e596cc3ee6333f9ee2f70c08aa4bbf36a4bd5f
Geutodun <eatere@naver.com>
2025-01-15 18:05:17 +0900

    배포판 추가

--- react-work/src/index.js
+++ react-work/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root') // public/index.html의 #root와 연결
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
